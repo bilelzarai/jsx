bb1743c5aba76568b9627e591e53302e453cbc36
Bilel Zarai <32228780+bilelzarai@users.noreply.github.com>
2021-10-13 14:42:44 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import imagesrc from "./imageInSrc.png"
+import video from './Altwork 1 Minute Video.MP4'
 import './App.css';
 import './style.css';
 
@@ -20,12 +21,12 @@ function App() {
         <img src="/imageInPublic.png" alt="image1" />
 
               </div>
-
+      <iframe width="320" height="240" src="https://www.youtube.com/embed/ngc9gnGgUdA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+             
               <video width="320" height="240" controls>
-
-        <source src='https://www.youtube.com/watch?v=aibtHnbeuio' type="video/mp4" />
-
+        <source src={video } type="video/mp4" />
               </video>
+
     </div>
   );
 }
